83e054912266818fae47baf0dbe3182508f44fbc
root <root@ovirt.mitko.local>
2016-07-10 00:07:35 +0300

    define Parser for the cli arguments

--- ovirtctl.py
+++ ovirtctl.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 import argparse
-from ovirtsdk.api import API
-from ovirtsdk.xml import params
+#from ovirtsdk.api import API
+#from ovirtsdk.xml import params
 
-VERSION = params.Version(major='3', minor='0')
+#VERSION = params.Version(major='3', minor='0')
+
+# define Parser for the cli arguments
 
 parser = argparse.ArgumentParser(description='ovirt opensource management tool')
 parser.add_argument('--createdc', help='create new data center')
@@ -12,4 +14,6 @@ parser.add_argument('--createclu', help='create new cluster')
 parser.add_argument('--addhosts', help='add new host(s) to cluster')
 
 args = parser.parse_args()
-print(args.accumulate(args.integers))
+
+
+
