1e58f3d34d759abc0c22468786223fd7b827f901
Dimitar Georgiev Dimitrov <dimitar-georgiev.dimitrov@dgeorgievd.com>
2016-07-09 23:49:13 +0300

    .

--- ovirtctl.py
+++ ovirtctl.py
@@ -7,9 +7,9 @@ from ovirtsdk.xml import params
 VERSION = params.Version(major='3', minor='0')
 
 parser = argparse.ArgumentParser(description='ovirt opensource management tool')
-parser.add_argument('createdc', help='create new data center')
-parser.add_argument('createclu', help='create new cluster')
-parser.add_argument('addhosts', help='add new host(s) to cluster')
+parser.add_argument('--createdc', help='create new data center')
+parser.add_argument('--createclu', help='create new cluster')
+parser.add_argument('--addhosts', help='add new host(s) to cluster')
 
 args = parser.parse_args()
 print(args.accumulate(args.integers))
